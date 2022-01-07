cd1a5e39cf72a582e2ce91e40b488afcbb6244c9
Viktor <vivchy79@gmail.com>
2022-01-07 23:21:36 +0400

    begin

---
+++ main.py
@@ -0,0 +1,23 @@
+import telebot
+# импорт auth_data.py переменной token
+from auth_data import token
+
+
+def get_telebot():
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=['start', 'help'])
+    def send_welcome(message):
+        bot.reply_to(message, message.text)
+
+    @bot.message_handler(content_types=['text'])
+    def send_name(message):
+        bot.reply_to(
+            message,
+            f"Welcome {message.from_user.first_name} what does this '{message.text}' mean ?")
+
+    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    get_telebot()
